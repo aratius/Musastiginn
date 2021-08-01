a2adde739155f8fd19af5aa94f9768247c427186
aratius <arata1129matsu@icloud.com>
2021-08-01 13:29:05 +0900

    指紋認証

--- TurnOnDaikinAC/TurnOnDaikinAC.ino
+++ TurnOnDaikinAC/TurnOnDaikinAC.ino
@@ -6,12 +6,14 @@
 #include <ir_Daikin.h>
 #include <M5StickCPlus.h>
 
-const uint16_t kIrLed = 9;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+const uint16_t kIrLed = 32;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 IRDaikin176 ac(kIrLed);  // Set the GPIO to be used to sending the message
+bool isOn = false;
 
 void setup() {
   ac.begin();
   Serial.begin(115200);
+  isOn = ac.getPower();
 
   M5.begin();
   M5.Lcd.print("ac ctrller");
@@ -20,34 +22,36 @@ void setup() {
 void loop() {
   M5.update();
 
-	if(M5.BtnA.wasPressed() || M5.BtnB.wasPressed()) {
+	if(M5.BtnA.wasPressed()) {
     // M5.Lcd.clear(TFT_WHITE);
     M5.Lcd.println("Sending...");
 
-    // Set up TFT_WHITE we want to send. See ir_Daikin.cpp for all the options.
-    ac.on();
-    ac.setFan(1);
-    ac.setMode(kDaikinCool);
-    ac.setTemp(25);
-//    ac.setSwingVertical(false);
-    ac.setSwingHorizontal(false);
-
-    // Set the current time to 1:33PM (13:33)
-    // Time works in minutes past midnight
-//    ac.setCurrentTime(13 * 60 + 33);
-    // Turn off about 1 hour later at 2:30PM (14:30)
-//    ac.enableOffTimer(14 * 60 + 30);
-
-    // Display what we are going to send.
-    M5.Lcd.println(ac.toString());
+    turn();
 
     // Now send the IR signal.
 #if SEND_DAIKIN176
     ac.send();
 #endif  // SEND_DAIKIN
-    ac.send();
-    M5.Lcd.println("did not send");
 
   }
 
+}
+
+void turn() {
+  if(isOn) {
+    ac.off();
+  } else {
+    ac.on();
+  }
+
+  isOn = !isOn;
+
+  ac.setFan(1);
+  ac.setMode(kDaikinCool);
+  ac.setTemp(25);
+  ac.setSwingHorizontal(false);
+
+  // Display what we are going to send.
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.println(isOn);
 }
